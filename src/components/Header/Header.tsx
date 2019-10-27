7fe7293efd7acd4b38318dffa6dfd606b474bee8
bugong <1761869346@qq.com>
2019-10-27 22:49:45 +0800

    edit: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Layout, Icon} from 'antd';
+import {Layout, Icon, Menu, Avatar, Popover, Badge} from 'antd';
 import styles from './Header.module.scss';
+
+const SubMenu = Menu.SubMenu;
 
 class Header extends React.PureComponent<any> {
     render() {
@@ -9,7 +11,31 @@ class Header extends React.PureComponent<any> {
                 <div className={styles.button}>
                     <Icon type={'menu-unfold'} />
                 </div>
-                <div>Header</div>
+                <div className={styles.operationContainer}>
+                    <Popover>
+                        <Badge className={styles.notice}
+                               dot
+                               offset={[-10, 10]}
+                        >
+                            <Icon type={'bell'} />
+                        </Badge>
+                    </Popover>
+
+                    <Menu key={'user'} mode={'horizontal'}>
+                        <SubMenu title={
+                            <>
+                                <span style={{
+                                    marginRight: '5px'
+                                }}>管理员</span>
+                                <Avatar></Avatar>
+                            </>
+                        }>
+                            <Menu.Item>
+                                退出登录
+                            </Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </div>
             </Layout.Header>
         );
     }
